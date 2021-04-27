13d4d148efa12ed4271d5b68e5bc4ff25f61d937
Mikhail Rubanov <m.rubanov@dodopizza.com>
2021-04-27 16:05:15 +0300

    Update

--- TestParser/Sources/TestParser/TestParser.swift
+++ TestParser/Sources/TestParser/TestParser.swift
@@ -1,3 +1,80 @@
-struct TestParser {
-    var text = "Hello, World!"
+import Foundation
+
+class JSONFailParser {
+    
+    let filePath: URL
+    
+    init(filePath: URL) {
+        self.filePath = filePath
+    }
+    
+    func parse() throws -> Report {
+        let data = try Data(contentsOf: filePath)
+        let report: Report = try JSONDecoder().decode(Report.self, from: data)
+        
+        return report
+    }
+    
+    func failedNames() throws -> [String] {
+        let report = try parse()
+        return report.issues.testFailureSummaries._values.map { value in
+            return value.testCaseName._value
+        }
+        
+    }
+}
+
+public class ReportParser {
+    let folder: URL
+    
+    public init(folder: URL) {
+        self.folder = folder
+    }
+    
+    public func parse() throws -> [String] {
+        
+//        shell("ls")
+//
+//        let command = "xcrun xcresulttool get --path E2ETests.xcresult --format json"
+//        shell(command)
+        
+        let json = folder.appendingPathComponent("report.json")
+        
+        let failedTests = try JSONFailParser(filePath: json).failedNames()
+        
+        return failedTests
+    }
+}
+
+
+
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
+
+struct Report: Codable {
+    let issues: Issues
+}
+
+struct Issues: Codable {
+    let testFailureSummaries: TestFailureSummaries
+}
+
+struct TestFailureSummaries: Codable {
+    let _values: [FailureValue]
+}
+
+struct FailureValue: Codable {
+    let testCaseName: TestCaseName
+}
+
+struct TestCaseName: Codable {
+    let _value: String
 }
--- TestReportParser/main.swift
+++ TestReportParser/main.swift
@@ -5,7 +5,13 @@
 //  Created by Станислав Карпенко on 23.04.2021.
 //
 
+import TestParser
 import Foundation
 
-print("Hello, World!")
+let path = CommandLine.arguments[1]
+let url = URL(fileURLWithPath: path)
+let parser = ReportParser(folder: url)
+let result = try! parser.parse()
+print("\(result)")
+
 
