70ebeabe6d0c0b8fb101df8903871ceeb007ec23
Rubanov Mikhail <m.rubanov@dodopizza.com>
2021-05-12 19:48:36 +0500

    Issues become optional

--- TestParser/Tests/TestParserTests/TestParserTests.swift
+++ TestParser/Tests/TestParserTests/TestParserTests.swift
@@ -14,13 +14,29 @@ final class TestParserTests: XCTestCase {
 
         let report = try parser.parse()
 
-        XCTAssertEqual(report.issues.testFailureSummaries._values[0].testCaseName._value,
+        XCTAssertEqual(report.issues.testFailureSummaries?._values[0].testCaseName._value,
                        "AuthorizationTests.test_guest_can_login_in_russia_with_lithuania_phone()")
-        XCTAssertEqual(report.issues.testFailureSummaries._values[1].testCaseName._value,
+        XCTAssertEqual(report.issues.testFailureSummaries?._values[1].testCaseName._value,
                        "AuthorizationTests.test_guest_can_login_in_russia_with_estonia_phone()")
 
         let names = ["AuthorizationTests.test_guest_can_login_in_russia_with_lithuania_phone()",
                      "AuthorizationTests.test_guest_can_login_in_russia_with_estonia_phone()"]
+        XCTAssertEqual(try parser.failedNames(),
+                       names)
+        
+        
+    }
+    
+    func testExample2() throws {
+        let reportPath = try XCTUnwrap(Bundle.module.url(forResource: "reportUnitsFailure", withExtension: "json"))
+        let parser = JSONFailParser(filePath: reportPath)
+        
+        let report = try parser.parse()
+        
+        XCTAssertEqual(report.issues.testFailureSummaries?._values[0].testCaseName._value,
+                       "DownloadImageServiceSpec.DownloadImageService__prefetchFirstSmallImagesForAllCategories__when_not_2G__it_should_prefetch()")
+        
+        let names = ["DownloadImageServiceSpec.DownloadImageService__prefetchFirstSmallImagesForAllCategories__when_not_2G__it_should_prefetch()"]
         XCTAssertEqual(try parser.failedNames(),
                        names)
         
