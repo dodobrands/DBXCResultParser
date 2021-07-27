1fee435834e008609c4b7650de303ef32912b753
Stanislav Karpenko <s.karpenko@dodopizza.com>
2021-07-27 14:55:40 +0300

    added modes

--- TestReportParser/main.swift
+++ TestReportParser/main.swift
@@ -5,13 +5,26 @@
 //  Created by Станислав Карпенко on 23.04.2021.
 //
 
-import TestParser
 import Foundation
+import TestParser
 
 let path = CommandLine.arguments[1]
+let mode = CommandLine.arguments[2]
 let url = URL(fileURLWithPath: path)
 let parser = ReportParser(filePath: url)
-let result = try! parser.parse()
+
+let result: String
+switch mode {
+case "total":
+    result = try! parser.parseTotalTests()
+case "skipped":
+    result = try! parser.parseSkippedTests()
+case "failed":
+    result = try! parser.parseFailedTests()
+case "list":
+    result = try! parser.parseList()
+default:
+    result = "Unknow argument"
+}
+
 print("\(result)")
-
-
