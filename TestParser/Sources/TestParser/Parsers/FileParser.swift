6e5bd22ac1d0f06e540a34e4d55f9f9c412e7c92
Aleksey Berezka <a.berezka@dodopizza.com>
2021-12-30 13:57:03 +0500

    Moved files

---
+++ TestParser/Sources/TestParser/Parsers/FileParser.swift
@@ -0,0 +1,20 @@
+//
+//  FileParser.swift
+//  
+//
+//  Created by Алексей Берёзка on 30.12.2021.
+//
+
+import Foundation
+
+class FileParser {
+    let filePath: URL
+    
+    init(filePath: URL) {
+        self.filePath = filePath
+    }
+    
+    func data() throws -> Data {
+        try Data(contentsOf: filePath)
+    }
+}
