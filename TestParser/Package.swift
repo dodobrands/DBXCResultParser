9a4ddfb67dd74e8cf5941cc89d83bb21ab6f05b7
Aleksey Berezka <a.berezka@dodopizza.com>
2021-12-30 15:51:18 +0500

    Turned off XMLCoder

--- TestParser/Package.swift
+++ TestParser/Package.swift
@@ -15,14 +15,16 @@ let package = Package(
             targets: ["TestParser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.12.0")
+//        .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.12.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TestParser",
-            dependencies: ["XMLCoder"]),
+            dependencies: [
+//                "XMLCoder"
+            ]),
         .testTarget(
             name: "TestParserTests",
             dependencies: ["TestParser"],
