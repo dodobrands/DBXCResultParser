d49abaa25e90fd6b159a1b860846c7839de9b14f
Aleksey Berezka <a.berezka@dodopizza.com>
2022-01-05 19:25:47 +0500

    Marking slow tests

---
+++ Sources/TestReportParser/Models/Math.swift
@@ -0,0 +1,22 @@
+//
+//  Math.swift
+//  
+//
+//  Created by Алексей Берёзка on 05.01.2022.
+//
+
+import Foundation
+
+extension Sequence where Element: AdditiveArithmetic {
+    /// Returns the total sum of all elements in the sequence
+    func sum() -> Element {
+        reduce(.zero, +)
+    }
+}
+
+extension Collection where Element: BinaryFloatingPoint {
+    /// Returns the average of all elements in the array
+    func average() -> Element {
+        isEmpty ? .zero : sum() / Element(count)
+    }
+}
