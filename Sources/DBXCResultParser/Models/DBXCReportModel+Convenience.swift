ae26d84b86d49657cf9e17ee4638a290d13a97b8
Aleksey Berezka <a.berezka@dodopizza.com>
2023-12-18 19:45:13 +0500

    Filter is configurable

--- Sources/DBXCResultParser/Models/DBXCReportModel+Convenience.swift
+++ Sources/DBXCResultParser/Models/DBXCReportModel+Convenience.swift
@@ -10,10 +10,18 @@ import Foundation
 extension DBXCReportModel {
     /// Initializes a new instance of the `DBXCReportModel` using the provided `xcresultPath`.
     /// The initialization process involves parsing the `.xcresult` file to extract various reports.
+    /// Coverage data for targets specified in `excludingCoverageNames` will be excluded from the report.
     ///
-    /// - Parameter xcresultPath: The file URL of the `.xcresult` file to parse.
+    /// - Parameters:
+    ///   - xcresultPath: The file URL of the `.xcresult` file to parse.
+    ///   - excludingCoverageNames: An array of strings representing the names of the targets to be excluded 
+    ///                             from the code coverage report. Defaults to an empty array, meaning no 
+    ///                             targets will be excluded.
     /// - Throws: An error if the `.xcresult` file cannot be parsed or if required data is missing.
-    public init(xcresultPath: URL) throws {
+    public init(
+        xcresultPath: URL,
+        excludingCoverageNames: [String] = []
+    ) throws {
         // Parse the overview report from the xcresult file, which contains general test execution information.
         let overviewReport = try OverviewReportDTO(from: xcresultPath)
         
@@ -22,11 +30,11 @@ extension DBXCReportModel {
         let detailedReport = try DetailedReportDTO(from: xcresultPath,
                                                    refId: overviewReport.testsRefId)
         
-        // Attempt to parse the code coverage data from the xcresult file.
-        // It excludes any coverage data related to test helpers and test cases themselves.
+        // Attempt to parse the code coverage data from the xcresult file, excluding specified targets.
         let coverageDTOs = try? Array<CoverageDTO>(from: xcresultPath)
-            .filter { !$0.name.contains("TestHelpers") && !$0.name.contains("Tests") }
+            .filter { !excludingCoverageNames.contains($0.name) }
         
+        // Initialize the DBXCReportModel with the parsed report data, including the filtered coverage data.
         self = try DBXCReportModel(
             overviewReportDTO: overviewReport,
             detailedReportDTO: detailedReport,
