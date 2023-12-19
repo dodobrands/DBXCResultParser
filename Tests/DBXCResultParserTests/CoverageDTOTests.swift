61e8a1a4d29a86f785fb640b2eee03949d3325a9
Aleksey Berezka <a.berezka@dodopizza.com>
2023-12-19 11:06:57 +0500

    Added tests

--- Tests/DBXCResultParserTests/CoverageDTOTests.swift
+++ Tests/DBXCResultParserTests/CoverageDTOTests.swift
@@ -1,7 +1,6 @@
 // Created by Yaroslav Bredikhin on 06.09.2022
 
 import Foundation
-import Foundation
 import XCTest
 @testable import DBXCResultParser
 
@@ -15,7 +14,24 @@ class CoverageDTOTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func test_file_with_coverage() throws {
-        XCTAssertNoThrow(try Array<CoverageDTO>(from: Constants.unitTestsWithCoverageReportPath))
+    func test_coverageDtoParse() throws {
+        XCTAssertNoThrow(try Array<CoverageDTO>(from: Constants.testsReportPath))
+    }
+    
+    func test_coverageDtoData() throws {
+        let result = try Array<CoverageDTO>(from: Constants.testsReportPath)
+        XCTAssertEqual(result.count, 3) // as targets count
+        let expectedResult = CoverageDTO.testMake(
+            coveredLines: 299,
+            executableLines: 582,
+            lineCoverage: 0.5137457044673539,
+            name: "DBXCResultParser"
+        )
+        
+        let target = try XCTUnwrap(result.first { $0.name == expectedResult.name })
+        
+        XCTAssertEqual(target.coveredLines, expectedResult.coveredLines)
+        XCTAssertEqual(target.executableLines, expectedResult.executableLines)
+        XCTAssertEqual(target.lineCoverage, expectedResult.lineCoverage)
     }
 }
