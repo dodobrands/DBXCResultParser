3aaca9c0e354d989b1a73c11dbd9c7528954898d
Aleksey Berezka <a.berezka@dodobrands.io>
2024-02-27 23:42:35 +0500

    Fixed paths with whitespaces

--- Sources/DBXCResultParser/Models/DTO/DTO+Helpers.swift
+++ Sources/DBXCResultParser/Models/DTO/DTO+Helpers.swift
@@ -10,7 +10,8 @@ import Foundation
 extension ActionsInvocationRecordDTO {
     init(from xcresultPath: URL) throws {
         let filePath = try Constants.actionsInvocationRecord
-        try DBShell.execute("xcrun xcresulttool get --path \(xcresultPath.relativePath) --format json > \(filePath.relativePath)")
+        let command = "xcrun xcresulttool get --path '\(xcresultPath.relativePath)' --format json > '\(filePath.relativePath)'"
+        try DBShell.execute(command)
         let data = try Data(contentsOf: filePath)
         try FileManager.default.removeItem(atPath: filePath.relativePath)
         self = try JSONDecoder().decode(ActionsInvocationRecordDTO.self, from: data)
@@ -21,7 +22,8 @@ extension ActionTestPlanRunSummariesDTO {
     init(from xcresultPath: URL, refId: String? = nil) throws {
         let refId = try (refId ?? ActionsInvocationRecordDTO(from: xcresultPath).testsRefId)
         let filePath = try Constants.actionTestPlanRunSummaries
-        try DBShell.execute("xcrun xcresulttool get --path \(xcresultPath.relativePath) --format json --id \(refId) > \(filePath.relativePath)")
+        let command = "xcrun xcresulttool get --path '\(xcresultPath.relativePath)' --format json --id '\(refId)' > '\(filePath.relativePath)'"
+        try DBShell.execute(command)
         let data = try Data(contentsOf: filePath)
         try FileManager.default.removeItem(atPath: filePath.relativePath)
         self = try JSONDecoder().decode(ActionTestPlanRunSummariesDTO.self, from: data)
@@ -32,7 +34,8 @@ extension ActionTestSummaryDTO {
     init(from xcresultPath: URL, refId: String? = nil) throws {
         let refId = try (refId ?? ActionsInvocationRecordDTO(from: xcresultPath).testsRefId)
         let filePath = try Constants.actionTestSummary
-        try DBShell.execute("xcrun xcresulttool get --path \(xcresultPath.relativePath) --format json --id \(refId) > \(filePath.relativePath)")
+        let command = "xcrun xcresulttool get --path '\(xcresultPath.relativePath)' --format json --id '\(refId)' > '\(filePath.relativePath)'"
+        try DBShell.execute(command)
         let data = try Data(contentsOf: filePath)
         try FileManager.default.removeItem(atPath: filePath.relativePath)
         self = try JSONDecoder().decode(ActionTestSummaryDTO.self, from: data)
@@ -42,7 +45,8 @@ extension ActionTestSummaryDTO {
 extension Array where Element == CoverageDTO {
     init(from xcresultPath: URL) throws {
         let tempFilePath = try Constants.actionsInvocationRecord
-        try DBShell.execute("xcrun xccov view --report --only-targets --json \(xcresultPath.relativePath) > \(tempFilePath.relativePath)")
+        let command = "xcrun xccov view --report --only-targets --json '\(xcresultPath.relativePath)' > '\(tempFilePath.relativePath)'"
+        try DBShell.execute(command)
         let data = try Data(contentsOf: tempFilePath)
         try FileManager.default.removeItem(atPath: tempFilePath.relativePath)
         self = try JSONDecoder().decode(Array<CoverageDTO>.self, from: data)
