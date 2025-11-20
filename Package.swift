6ca8c8aaaa24257e187d73f743ad457061208029
Aleksey Berezka <a.berezka@dodobrands.io>
2025-11-20 13:18:33 +0500

    Fix case mismatch: rename executable target from peekie to Peekie to match directory name

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let executableLibraryName = "peekie"
 let testHelpersLibraryName = "PeekieTestHelpers"
 
 let parserTargetName = parserLibraryName
-let executableTargetName = executableLibraryName
+let executableTargetName = "Peekie"
 let testHelpersTargetName = testHelpersLibraryName
 
 let parserTestsTargetName = parserTargetName + "Tests"
