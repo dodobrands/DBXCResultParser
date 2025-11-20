6c492483aa5511fa756ec2e34addf836ae99b6ba
Aleksey Berezka <a.berezka@dodobrands.io>
2025-11-20 13:17:44 +0500

    Restore explicit path for test target - required when target name differs from directory name

--- Package.swift
+++ Package.swift
@@ -107,6 +107,7 @@ let package = Package(
                     package: "swift-snapshot-testing"
                 ),
             ],
+            path: "Tests/PeekieTests",
             exclude: [
                 "__Snapshots__"
             ],
